ef3419c3356d2533f7c70d3626c16d4ab9a54c70
felerminoali <felerminoali@gmail.com>
2016-12-26 18:25:53 +0800

    message

--- PortalEmprego_Wth_MemberShip_And_mvc5/Controllers/HomeController.cs
+++ PortalEmprego_Wth_MemberShip_And_mvc5/Controllers/HomeController.cs
@@ -29,14 +29,10 @@ namespace PortalEmprego_Wth_MemberShip_And_mvc5.Controllers
                             join lc in db.Locals on an.Location equals lc.Id
                             group new { an,lc} by new {lc.Name, lc.Id} into newt
                             select new { Id = newt.Key.Id, Name = newt.Key.Name, Count = newt.Count() });
-
-
-
             ViewBag.Location = localQuery.ToList<object>();
 
 
             // filtrar apenas as categorias
-
             var categoryQuery = (
                                 from an in db.Anuncios
                                 join cat in db.Categories on an.category equals cat.Id
@@ -44,47 +40,11 @@ namespace PortalEmprego_Wth_MemberShip_And_mvc5.Controllers
                                 select new { Id = newt.Key.Id, Name = newt.Key.Name, Count = newt.Count()}
 
                 );
-
             ViewBag.category = categoryQuery.ToList<object>();
-
-            //ViewBag.Location = db.Locals.Where(x => x.Provincia.HasValue).ToList();
-
-            //ViewBag.category = db.Categories.Where(x => !x.CatParent.HasValue).ToList();
-
-            //ViewBag.category = db.Categories.OrderBy(a=>a.Name).ToList();
-
-
 
             ViewBag.totalAnuncios = db.Anuncios.Count();
             return View(db.Anuncios.ToList().ToPagedList(page ?? 1, 8));
-            //return View();
         }
 
-        [Authorize]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-
-
-
-        public JsonResult PieChart()
-        {
-
-            var chartsdata = db.Anuncios.Select(a => new { a.Local.Name, a.Location });
-
-
-            return Json(chartsdata, JsonRequestBehavior.AllowGet);
-        }
     }
 }
